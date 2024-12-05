07c34c80b4efbc0ec5e7d68458ac20b9121e84c6
J56164 <jjclub12099@gmail.com>
2024-12-05 14:37:43 +0700

    Appending paper data into CSV

---
+++ data_append.py
@@ -0,0 +1,82 @@
+# %%
+import json
+import multiprocessing
+import pathlib
+from typing import List
+
+import pandas as pd
+
+
+# %%
+def get(data: dict, path: str | List[str]):
+    if type(path) is not list:
+        path = path.split(".")
+    for property in path:
+        if type(data) is not dict or property not in data:
+            return None
+        data = data[property]
+    return data
+
+
+# %%
+def get_data(paths: pathlib.Path):
+    data_list = []
+    for path in paths:
+        try:
+            with open(path, encoding="UTF-8") as file:
+                data = json.load(file)
+            filtered_data = {}
+            filtered_data["title"] = get(
+                data, ["abstracts-retrieval-response", "coredata", "dc:title"]
+            )
+            filtered_data["abstract"] = get(
+                data,
+                [
+                    "abstracts-retrieval-response",
+                    "item",
+                    "bibrecord",
+                    "head",
+                    "abstracts",
+                ],
+            )
+            filtered_data["publish_date"] = get(
+                data, ["abstracts-retrieval-response", "coredata", "prism:coverDate"]
+            )
+            filtered_data["cited_by_count"] = get(
+                data, ["abstracts-retrieval-response", "coredata", "citedby-count"]
+            )
+            filtered_data["reference_count"] = get(
+                data,
+                [
+                    "abstracts-retrieval-response",
+                    "item",
+                    "bibrecord",
+                    "tail",
+                    "bibliography",
+                    "@refcount",
+                ],
+            )
+            data_list.append(filtered_data)
+            print(f"Success: {path}")
+        except Exception as e:
+            print(f"Failed: {path}\nException: {repr(e)}")
+    return data_list
+
+
+# %%
+data_list = []
+
+# %%
+buckets = {i: [] for i in range(12)}
+
+for path in pathlib.Path("Data 2018-2023/Project").glob("**/*"):
+    buckets[path.__hash__() % 12].append(path)
+
+# %%
+if __name__ == "__main__":
+    cpu_count = multiprocessing.cpu_count()
+    with multiprocessing.Pool(cpu_count) as p:
+        for data_part in p.map(get_data, buckets.values()):
+            data_list += data_part
+    df = pd.DataFrame(data_list)
+    df.to_csv("papers.csv")
